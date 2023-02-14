e2dd8365b4752e71a0df56f9cead35113deb82fc
Shakira <shakiraayunda@gmail.com>
2023-02-14 10:09:26 +0700

    tambah

---
+++ src/main/java/com/mycompany/uas/Uas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.uas;
+
+/**
+ *
+ * @author Pluto 01
+ */
+public class Uas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/TestNGTest1.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/EmptyTestNGTest.java to edit this template
+ */
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import static org.openqa.selenium.By.className;
+import static org.openqa.selenium.By.id;
+import static org.openqa.selenium.By.xpath;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import static org.testng.Assert.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Pluto 01
+ */
+public class TestNGTest1 {
+    
+    WebDriver driver = null;
+
+    @BeforeTest
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @Test
+    public void firstTest(){
+        driver.get("http://localhost/AUTng/index.php");
+        driver.findElement(id("username")).sendKeys("admin");
+        driver.findElement(id("password")).sendKeys("admin123");
+        driver.findElement(xpath("/html/body/div/form/input[3]")).click();
+        String expectedResult = "Home Page Welcome back, admin!";
+        String actualResult = driver.findElement(className("content")).getText();
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    @AfterTest
+    public void tearDown(){
+        driver.close();
+        driver.quit();
+    }
+
+}
